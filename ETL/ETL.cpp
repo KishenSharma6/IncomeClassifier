d9a65f1f14b8d7743fc6d68da5b37c1440d774e8
KishenSharma6 <57292990+KishenSharma6@users.noreply.github.com>
2021-09-13 18:32:26 -0700

    Tested read _csv(), needs more updating

--- ETL/ETL.cpp
+++ ETL/ETL.cpp
@@ -3,9 +3,9 @@
 
 int main() {
     // Read three_cols.csv and ones.csv
-    std::vector<std::pair<std::string, std::vector<int>>> three_cols = read_csv("three_cols.csv");
-    std::vector<std::pair<std::string, std::vector<int>>> ones = read_csv("ones.csv");
+    std::vector<std::pair<std::string, std::vector<int>>> three_cols = read_csv("../Dataset/adultCensusIncome.csv");
+    
 
     
-    return 0;
+    return EXIT_SUCCESS;
 }
