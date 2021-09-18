c025e1612b7185807e7a44cf228e77b989a01620
KishenSharma6 <57292990+KishenSharma6@users.noreply.github.com>
2021-09-17 18:12:00 -0700

    Refactored

--- ETL/ETL.h
+++ ETL/ETL.h
@@ -5,18 +5,17 @@
 #include <fstream>
 #include <eigen3/Eigen/Dense>
 
-class ETL
-{
+class ETL{
     std::string dataset; //name/location of data
     std::string delimiter; //delimeter for reading file
     bool header; //Whether we have a header or not when reading in data
 
 public:
     //constructor
-    ETL(std::string data, std::string separator, bool head) : dataset(data), delimiter(separator), header(head)
-    {}
+    ETL(std::string data, std::string separator, bool head) : dataset(data), delimiter(separator), header(head){}
 
-    
+    std::vector<std::vector<std::string>> readCSV();
+
 };
 
 #endif
