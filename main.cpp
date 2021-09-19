314f81b947d465ddc5dfe370a616ed063519b207
KishenSharma6 <57292990+KishenSharma6@users.noreply.github.com>
2021-09-18 18:04:38 -0700

    Testing CSVtoEigen()

--- main.cpp
+++ main.cpp
@@ -8,11 +8,17 @@
 
 
 int main(int argc, char *argv[]){
-
+    //Create object if ETL that will include 3 input args of ETL: input name, delimiter, flag
     ETL etl(argv[1], argv[2], argv[3]);
      
-
+    //Get data from CSV file using readCSV method **NOTE** we dont have a way of storing this data yet
     std::vector<std::vector<std::string>> dataset = etl.readCSV();
+
+    int rows= dataset.size();
+    int cols= dataset[0].size();
+
+    Eigen::MatrixXd dataMat = etl.CSVtoEigen(dataset, rows, cols);
+
     return EXIT_SUCCESS;
 
 }
